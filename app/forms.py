d205a5b5eb14927d8f42e64d170db1b419302bbd
Mark Melnic <markhappy200@gmail.com>
2021-01-23 15:42:39 +0200

    Modified User form and added AddTable form

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from app.models import User
+from app.models import User, Table
 
 class LoginForm(FlaskForm):
-    username = StringField("Email", validators=[DataRequired(), Length(max=50)])
+    username = StringField("Username", validators=[DataRequired(), Length(max=50)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
-    remember = BooleanField("Remember")
+    remember = BooleanField("Remember me")
     submit = SubmitField("Sign in")
+
+class AddTable(FlaskForm):
+    number = IntegerField("Table Number", validators=[DataRequired(), Length(max=3)])
+    seats = IntegerField("Number of seats", validators=[Length(max=3)])
+    description = StringField("Number of seats", validators=[Length(max=50)])
+    add = SubmitField("Add")
