04b846b06d7b2ce8e65967fb1a8a14087f0a320f
Mark Melnic <markhappy200@gmail.com>
2021-02-23 23:02:12 +0200

    Database migrator added

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,16 +12,15 @@ app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["QRS_FOLDER"] = "app/static/img/qrcodes/"
-app.config["DSHES_FOLDER"] = "app/static/img/dishes/"
-app.config["CATGS_FOLDER"] = "app/static/img/categories/"
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 sass.compile(dirname=('app/static/scss', 'app/static/css'), output_style='compressed')
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate=Migrate(app,db) #Initializing migrate.
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
